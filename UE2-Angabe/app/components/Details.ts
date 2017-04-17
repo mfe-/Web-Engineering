b86bb42abafa82662ec586dbd75648793b4b0dfe
mfe userlast <martin.fetzel@get-the-solution.net>
2017-04-17 22:35:32 +0200

    First Draft Gerätedetailseite #19

--- UE2-Angabe/app/components/Details.ts
+++ UE2-Angabe/app/components/Details.ts
@@ -1,7 +1,29 @@
-import { Component } from "@angular/core";
+import { Component, Inject } from "@angular/core";
+import { DeviceService } from "../services/device.service";
+import { IUserService } from "../contracts/IUserService";
+import { Device } from "../model/device";
+import { ActivatedRoute, Router } from "@angular/router";
 
-@Component({  moduleId: module.id,  selector: 'Details',  templateUrl: 'Details.html'})
-export class Details
-{
-
+@Component({ moduleId: module.id, selector: 'Details', templateUrl: 'Details.html' })
+export class Details {
+    protected _DeviceService: DeviceService;
+    protected _IUserService: IUserService;
+    protected _Router: ActivatedRoute;
+    public constructor(private route: ActivatedRoute, deviceService: DeviceService, @Inject('IUserService') userservice: IUserService) {
+        this.Device = null;
+        this._IUserService = userservice;
+        this._DeviceService = deviceService;
+        this._Router = route;
+        this._Router.params.subscribe(this.GetDevice.bind(this));
+    }
+    public GetDevice(params: any): void {
+        var id: string = <string>params['id'];
+        console.log(id);
+        this._DeviceService.getDevice(id).then(this.SetDevice.bind(this));
+    }
+    private SetDevice(d: Device): void {
+        this.Device = d;
+        console.log(d);
+    }
+    public Device: Device = new Device();
 }
--- UE2-Angabe/app/components/Login.ts
+++ UE2-Angabe/app/components/Login.ts
@@ -1,18 +1,14 @@
-import { Component,Inject } from "@angular/core";
+import { Component, Inject } from "@angular/core";
 import { IUserService } from "../contracts/IUserService";
 
-@Component({  moduleId: module.id,  selector: 'Login',  templateUrl: 'Login.html' })
-export class Login
-{
-    protected _IUserService:IUserService;
-    public constructor(@Inject('IUserService') userservice:IUserService)
-    {
+@Component({ moduleId: module.id, selector: 'Login', templateUrl: 'Login.html' })
+export class Login {
+    protected _IUserService: IUserService;
+    public constructor( @Inject('IUserService') userservice: IUserService) {
         this._IUserService = userservice;
-        console.log(userservice);
     }
 
-    public get UserService():IUserService
-    {
+    public get UserService(): IUserService {
         return this._IUserService;
     }
 
