9ced5f95e3cc7eca80c7a8117104d79f40dc61e9
mfe userlast <martin.fetzel@get-the-solution.net>
2017-04-18 14:38:37 +0200

    Passwort ändern close #14

--- UE2-Angabe/app/components/Options.ts
+++ UE2-Angabe/app/components/Options.ts
@@ -1,19 +1,22 @@
-import { Component,Inject } from "@angular/core";
+import { Component, Inject } from "@angular/core";
 import { IUserService } from "../contracts/IUserService";
 
-@Component({  moduleId: module.id,  selector: 'Options',  templateUrl: 'Options.html' })
-export class Options
-{
-    protected _IUserService:IUserService;
-    public constructor(@Inject('IUserService') userservice:IUserService)
-    {
+@Component({ moduleId: module.id, selector: 'Options', templateUrl: 'Options.html' })
+export class Options {
+
+    protected _IUserService: IUserService;
+    public constructor( @Inject('IUserService') userservice: IUserService) {
         this._IUserService = userservice;
-        console.log(userservice);
     }
 
-    public get UserService():IUserService
-    {
+    public get UserService(): IUserService {
         return this._IUserService;
+    }
+    public ChangePassword(): void {
+
+    }
+    onSubmit(form: any): void {
+        console.log('you submitted value:', form);
     }
 
 }
