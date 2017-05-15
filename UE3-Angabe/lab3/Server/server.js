b6e0d214f6bc6167d163cd8e588efbc819e331d1
Martin Fetzel <martin.fetzel@get-the-solution.net>
2017-05-15 16:07:57 +0200

    Uebung 3 REST-Schnittstelle #23 base methods

--- UE3-Angabe/lab3/Server/server.js
+++ UE3-Angabe/lab3/Server/server.js
@@ -14,7 +14,7 @@ var jwt = require('jsonwebtoken');
 var cors = require('cors');
 var uuid = require('uuid');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
@@ -44,11 +44,131 @@ app.post("/updateCurrent", function (req, res) {
      * Diese Funktion verändert gleichzeitig auch den aktuellen Wert des Gerätes, Sie müssen diese daher nur mit den korrekten Werten aufrufen.
      */
 });
+// Create -> Post
+// Read   -> Get
+// Update -> Put
+// Delete -> Delete
+
+app.get('/user/:id', function (req, res, next) {
+    console.log('get Parameter:', req.params.id);
+    res.send(200, app.users);
+    next();
+});
+app.get('/status/', function (req, res, next) {
+    res.status(200).send({ start: app.start, failedLogins: app.failedLogin });
+});
+app.put('/updatePassword/', function (req, res, next) {
+    console.log("todo update password");
+});
+app.get('/devices/:id*?', function (req, res, next) { //*? - optionaler param
+    console.log('get Parameter:', req.params.id);
+
+    if (req.params.id) {
+        var d = null;
+        app.devices.forEach(function (element) {
+            if (element.id == req.params.id) {
+                d = element;
+            }
+        }, this);
+        res.status(200).send(d);
+    }
+    else {
+        res.status(200).send(app.devices);
+    }
+
+    next();
+});
+
+app.post('/device/:id*?', function (req, res, next) {
+    //use xxx-url-encoded in postman
+    console.log('post Parameter:', req.body.id);
+    if (req.body.id) {
+        req.body.id = uuid.v1();
+        app.devices.push(req.body);
+        res.status(201).send(req.body);
+    }
+    else {
+        // Bad Request
+        res.status(400);
+    }
+    next();
+});
+app.delete('/device/:id', function (req, res, next) {
+    if (req.body.id) {
+        console.log('delete Parameter:', req.params.id);
+        var d = null;
+        app.devices.forEach(function (element) {
+            if (element.id == req.params.id) {
+                d = element;
+            }
+        }, this);
+        console.log('Found device:', d);
+        if (d == null) {
+            res.status(400);
+        }
+        else {
+            // console.log(app.devices.length);
+            var index = app.devices.indexOf(d);
+            if (index > -1) {
+                app.devices.splice(index, 1);
+                console.log('removed device:', d);
+                // console.log(app.devices.length);
+                //reset content
+                res.status(205);
+            }
+        }
+
+    }
+    else {
+        // Bad Request
+        res.status(400);
+    }
+    next();
+});
+app.put('/device/:id*?', function (req, res, next) {
+    if (req.body.id) {
+        console.log('put Parameter:', req.body.id);
+        var d = null;
+        app.devices.forEach(function (element) {
+            if (element.id == req.body.id) {
+                d = element;
+            }
+        }, this);
+
+        if (d != null) {
+            var index = app.devices.indexOf(d);
+            if (index !== -1) {
+                app.devices[index] = req.body;
+                res.status(205);
+                next();
+            }
+        }
+        res.status(500);
+    }
+    else {
+        res.status(400);
+    }
+    next();
+});
 
 
 function readUser() {
     "use strict";
     //TODO Lesen Sie die Benutzerdaten aus dem login.config File ein.
+    fs.readFile('resources/login.config', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        //contributes to http://stackoverflow.com/questions/23558907/convert-text-file-into-clean-json-format
+        var file = data.split(/\r?\n/).reduce(function (m, i) {
+            var s = i.split(':');
+            m[s.shift()] = s.join(':');
+            return m;
+        }, {});
+        app.users = file;
+        console.log("users", app.users);
+    });
+
 }
 
 function readDevices() {
@@ -59,6 +179,15 @@ function readDevices() {
      *      simulation.simulateSmartHome(devices.devices, refreshConnected);
      * Der zweite Parameter ist dabei eine callback-Funktion, welche zum Updaten aller verbundenen Clients dienen soll.
      */
+
+    fs.readFile('resources/devices.json', 'utf8', function (err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        data = JSON.parse(data);
+        app.devices = data.devices;
+        console.log("devices", app.devices);
+    });
 }
 
 
@@ -82,6 +211,8 @@ var server = app.listen(8081, function () {
 
     var host = server.address().address;
     var port = server.address().port;
+    app.start = new Date();
+    app.failedLogin = 0;
     console.log("Big Smart Home Server listening at http://%s:%s", host, port);
 });
 
